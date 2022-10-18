a1c3eff5a1da2f8f6480628307f3e1fe0aadf294
Wesley Lin <wesley.lin@kcl.ac.uk>
2022-10-18 17:52:24 +0100

    Changes to migrations

--- things/models.py
+++ things/models.py
@@ -1,4 +1,5 @@
 from enum import unique
+from wsgiref.validate import validator
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
@@ -7,4 +8,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
-    quantity = models.IntegerField(unique=False)
+    quantity = models.IntegerField(unique=False,
+                                   validators=[MaxValueValidator(100, "Can't be over 100"),
+                                              MinValueValidator(0, "Can't be below 0")])
